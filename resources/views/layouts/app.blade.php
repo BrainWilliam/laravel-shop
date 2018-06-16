bebc3515f85d20fe3847ab74c27a94c9f927bb6b
vagrant <vagrant@homestead>
2018-06-16 15:51:23 +0000

    首页布局

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,20 @@
+<html>
+	<head>
+		<meta charset="utf-8">
+	    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+	    <meta name="viewport" content="width=device-width, initial-scale=1">
+		<title>@yield('title','laravle')--小魔仙电商</title>
+		<meta name="_token" content="{{csrf_token()}}">
+		<link rel="stylesheet" href="{{mix('css/app.css')}}">
+	</head>
+	<body>
+		<div id="app" class="{{route_class()}}-page">
+			@include('layouts._header')
+			<div class="container">
+				@yield('content')
+			</div>
+			@include('layouts._footer')
+		</div>
+		<script src="{{mix('js/app.js')}}"></script>
+	</body>
+</html>
